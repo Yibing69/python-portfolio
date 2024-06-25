6e3d0640fbbdcf18f6400291aa366e412b14e4f8
Yibing <>
2024-06-25 15:43:50 +0200

    past testcase factorial 4 = 24

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,5 +1,7 @@
 def factorial(n):
     if n <= 2:
         return n
-    else:
+    elif n <= 3:
         return n*2
+    elif n <= 4:
+        return n * 6
--- tests/test_factorial/test_factorial.py
+++ tests/test_factorial/test_factorial.py
@@ -5,3 +5,4 @@ def test_factorial():
     assert factorial(1) == 1
     assert factorial(2) == 2
     assert factorial(3) == 6
+    assert factorial(4) == 24
