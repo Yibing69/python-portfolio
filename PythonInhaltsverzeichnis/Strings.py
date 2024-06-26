3a74bf84cfc794594a2150b549dd42ae8e0354cc
Yibing <>
2024-06-26 15:19:34 +0200

    String

---
+++ PythonInhaltsverzeichnis/Strings.py
@@ -0,0 +1,16 @@
+# Strings sind text-based data (Buchstaben)
+
+beispiel = "Hello World"
+print(beispiel[0])
+# Printet den ersten String des Variabels "beispiel" (Output: "H")
+
+print(beispiel[-1])
+# Printet den letzten String des Variabels "beispiel" (Output: "d")
+
+print(beispiel[0:])
+# Printet den String von Index 0 bis zum letzten Index (Output: Hello World)
+
+
+alter = int(input("Wie alt bist du?"))
+print(alter)
+# Wenn man im Input eine Zahl eingibt, wird die Zahl kein Integer sein sonder ein String deswegen "int(input())
