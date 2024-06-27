9d1a69dfd6b8c14a48d4930c6b483c1da5dca500
Yibing <>
2024-06-27 11:13:50 +0200

    Formatted String

---
+++ PythonInhaltsverzeichnis/FormattedString.py
@@ -0,0 +1,16 @@
+# Beispiel
+name = "John"
+age = 30
+greeting = f"Hello, {name}. You are {age} years old."
+print(greeting)  # Output: Hello, Alice. You are 30 years old.
+
+name2 = "Bob"
+age2 = 25
+greeting2 = "Hello, {}. You are {} years old.".format(name2, age2)
+print(greeting2)  # Output: Hello, Bob. You are 25 years old.
+
+name3 = "Alice"
+age3 = 30
+pi = 3.14159
+message = f"Name: {name3}, Age: {age3}, Pi: {pi:.2f}"
+print(message)  # Output: Name: Alice, Age: 30, Pi: 3.14
