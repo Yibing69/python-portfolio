3e5b5281f57dc3602334e6a6a483bb603ba731cb
Yibing <>
2024-06-27 12:40:08 +0200

    Operator Precedence

---
+++ PythonInhaltsverzeichnis/Operator Precedence.py
@@ -0,0 +1,11 @@
+# Beispiel
+x = 10 + 3 * 2 ** 2  # ** = Quadrieren
+print(x)  # Output: 22
+
+y = (10 + 3) * 2 ** 2  # Zuerst 10 + 3 gerechnet wegen der Klammer
+print(y)  # Output: 52
+
+
+# Ab/Aufrunden
+z = 2.9
+print(round(z))
