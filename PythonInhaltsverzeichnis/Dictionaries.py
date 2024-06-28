4629a885e356b31c641e703f61e8f88cf669c375
Yibing <>
2024-06-28 11:39:35 +0200

    Dictionary

--- PythonInhaltsverzeichnis/Dictionaries.py
+++ PythonInhaltsverzeichnis/Dictionaries.py
@@ -8,3 +8,16 @@ print(customer.get("birthdate"))  # Gets the value from "birthdate" // Output: "
 
 customer["birthdate"] = "Jan 1 1980"  # Adds "birthdate into "costumer" dictionary
 print(customer["birthdate"])  # Output: Jan 1 1980
+
+# Example
+phone = input("Phone")
+digits_mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four"
+}
+output = ""
+for x in phone:
+    output += digits_mapping.get(x, "!") + " "
+print(output)
