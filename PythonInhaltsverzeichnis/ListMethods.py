706050dc5d007c2758b29831ea45d16f81fbf32d
Yibing <>
2024-06-28 10:21:54 +0200

    List Methods

--- PythonInhaltsverzeichnis/ListMethods.py
+++ PythonInhaltsverzeichnis/ListMethods.py
@@ -4,7 +4,7 @@ number.remove(5)  # Removes "5" from the list
 
 number.pop(4)  # Removes the index 4 starting from 0
 
-number.shuffle() # Shuffles the list
+number.shuffle()  # Shuffles the list
 
 number.append(6)  # Adds "6" at the end of the list
 
@@ -12,8 +12,8 @@ number.reverse()  # Reverses the order of the list
 
 number.clear()  # Clears the list
 
-number.sort() # Sorts the list
+number.sort()  # Sorts the list
 
-number.insert()  # Adds an element in a specific position
+number.insert(0, 0)  # Adds an element in a specific position
 
-number.index()  # Returns the index of the first element with the specified value
+number.index(0)  # Returns the index of the first element with the specified value
